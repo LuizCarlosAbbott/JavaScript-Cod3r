57d80680897123c0ead5b4f96992d8e1c9d13162
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-06-30 15:44:27 -0300

    perf: ⚡️ Using parallel on Gulp

--- Web/exercicios.js/gulp/css/gulpfile.js
+++ Web/exercicios.js/gulp/css/gulpfile.js
@@ -1,4 +1,4 @@
-const { series } = require("gulp");
+const { parallel } = require("gulp");
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const uglifycss = require("gulp-uglifycss");
@@ -17,4 +17,4 @@ function copiarHTML() {
   return gulp.src("src/index.html").pipe(gulp.dest("build"));
 }
 
-exports.default = series(transformacaoCSS, copiarHTML);
+exports.default = parallel(transformacaoCSS, copiarHTML);
