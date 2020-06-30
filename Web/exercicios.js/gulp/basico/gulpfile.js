68315c419fd19645170b76fd3d2c0556ae66a679
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-06-30 13:48:29 -0300

    feat: 🎸 Copying using Gulp

--- Web/exercicios.js/gulp/basico/gulpfile.js
+++ Web/exercicios.js/gulp/basico/gulpfile.js
@@ -1,9 +1,25 @@
 const gulp = require("gulp");
-const series = gulp.series;
+const { series, parallel } = require("gulp");
+
+const antes1 = (cb) => {
+  console.log("Tarefa Antes 1!");
+  return cb();
+};
+
+const antes2 = (cb) => {
+  console.log("Tarefa Antes 2!");
+  return cb();
+};
 
 function copiar(cb) {
-  console.log("Tarefa de copiar!");
+  // gulp.src(["pastaA/arquivo1.txt", "pastaA/arquivo2.txt"])
+  gulp.src("pastaA/**/*.txt").pipe(gulp.dest("pastaB"));
   return cb();
 }
 
-module.exports.default = series(copiar);
+const fim = (cb) => {
+  console.log("Tarefa Fim!");
+  return cb();
+};
+
+module.exports.default = series(parallel(antes1, antes2), copiar, fim);
