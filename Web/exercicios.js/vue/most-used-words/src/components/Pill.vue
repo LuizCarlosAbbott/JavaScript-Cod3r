92e6255661eef980bbc60dff3b826a115b89ff99
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-07-04 19:09:21 -0300

    feat: 🎸 Home + Pill component

---
+++ Web/exercicios.js/vue/most-used-words/src/components/Pill.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>
+      {{ name }}
+    </span>
+    <span>
+      {{ amount }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+<style></style>
