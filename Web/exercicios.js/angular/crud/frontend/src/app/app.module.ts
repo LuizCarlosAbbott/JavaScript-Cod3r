034d1bc28e23b927207909c36442e89c132e3afc
Luiz Carlos Abbott Galvao Neto <luiz.n@adireto.com>
2020-10-11 15:22:53 -0300

    feat: 🎸 Create product form

--- Web/exercicios.js/angular/crud/frontend/src/app/app.module.ts
+++ Web/exercicios.js/angular/crud/frontend/src/app/app.module.ts
@@ -22,6 +22,10 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
 
 @NgModule({
   declarations: [
@@ -45,7 +49,10 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
