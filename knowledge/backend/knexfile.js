a37797f9b6e502cfe04032e690c667d57eae0e7e
Luiz Carlos Abbott Galvao Neto <luiz.n@adireto.com>
2020-11-02 17:10:17 -0300

    perf: ⚡️ Adding db configs in env

--- knowledge/backend/knexfile.js
+++ knowledge/backend/knexfile.js
@@ -1,12 +1,8 @@
+const { db } = require("./.env");
+
 module.exports = {
   client: "postgresql",
-  connection: {
-    host: "0.0.0.0",
-    port: 5432,
-    database: "knowledge",
-    user: "postgres",
-    password: "postgres",
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10,
