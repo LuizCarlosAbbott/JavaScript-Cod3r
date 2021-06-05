f98a58002196cd57ee7ddc19dbc12db41816ea6d
Luiz Carlos Abbott Galvao Neto <luiz.n@adireto.com>
2021-06-05 13:23:40 -0400

    feat: Home component

--- knowledge/frontend/src/main.js
+++ knowledge/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from "./config/router";
 
 Vue.config.productionTip = false;
 
+require("axios").defaults.headers.common["Authorization"] =
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBlbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjIyOTEzMjU4LCJleHAiOjE2MjMxNzI0NTh9.x9WMLK1aqABu2rZ453rulCpzGLR-QUutBn3awwrZ2IE";
+
 new Vue({
   store,
   router,
